b2ca51b0335330866a4545a4b184823b7319e883
Jake Mackie <jakemackie31@gmail.com>
2024-02-12 22:50:02 +0000

    Learnt some data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let name: &str = "Jake";
+    let age: i32 = 18;
+    let teenager: bool = true;
+    let height: f32 = 6.0;
+
+    let is_teenager: &str;
+
+    if teenager {
+        is_teenager = "I am a teenager";
+    } else {    
+        is_teenager = "I am not a teenager";
+    }
+
+    println!("My name is {}. {} and I am {} years old. I am {} feet tall.", name, is_teenager, age, height);
 }
